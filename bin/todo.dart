06c310bcd1109e3da03c6fd16e233c28fe3f9032
francisco <fcootzg@gmail.com>
2020-11-07 21:43:41 -0700

    Almost finished

--- bin/todo.dart
+++ bin/todo.dart
@@ -1,5 +1,43 @@
 import 'package:todo/todo.dart' as todo;
+import 'package:args/args.dart';
+import 'dart:io';
+
+ArgResults argResults;
 
 void main(List<String> arguments) {
-  print('Hello world: ${todo.calculate()}!');
+  exitCode = 0;
+  var write = false;
+  var remove = false;
+  var helpmsg = false;
+
+  final parser = ArgParser()
+      ..addFlag('add', abbr: 'a', help: 'Add a task', callback: (add) {
+        if (add)  write = true;
+      })
+      ..addFlag('show', abbr: 's', help: 'Show the tasks list', callback: (show) {
+        if (show) todo.show();
+      })
+      ..addFlag('did', abbr: 'd', help: 'Remove a task', callback: (did) {
+        if (did) remove = true;
+      })
+      ..addFlag('help', abbr: 'h', help: 'Display help message', callback: (help) {
+        if (help) helpmsg = true;
+      });
+
+
+  argResults = parser.parse(arguments);
+  final task = argResults.rest;
+
+  var taskString = task.join(" ");
+
+  if (write) {
+    todo.write(taskString);
+  } else if (remove) {
+    todo.remove(int.parse(taskString));
+  } else if (helpmsg) {
+    print(parser.usage);
+  } 
+
 }
+
+
--- lib/todo.dart
+++ lib/todo.dart
@@ -1,3 +1,57 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:async';
+import 'dart:io';
+import 'dart:convert';
+
+final path = '/home/franciscoortiz/Desktop/Dart/todo/DB/db.txt';
+final file = File('/home/franciscoortiz/Desktop/Dart/todo/DB/db.txt');
+
+
+void show() async {
+  var lineNumber = 0;
+  final lines = utf8.decoder
+      .bind(file.openRead())
+      .transform(const LineSplitter());
+  try {
+    await for (var line in lines) {
+      stdout.write('${lineNumber++}. ');
+      stdout.writeln(line);
+    }
+  } catch(_) {
+    await _handleError(path);
+  }
 }
+
+
+void write(String task) {
+  var sink = file.openWrite(mode: FileMode.append); 
+  sink.writeln(task);
+
+  sink.close();
+
+}
+
+
+void remove(int index) {
+  file.readAsLines().then((List<String> lines) {
+    lines.removeAt(index);
+    final newTextData = lines.join('\n');
+    file.writeAsString(newTextData);
+  });
+}
+
+
+Future _handleError(String path) async {
+  if (await FileSystemEntity.isDirectory(path)) {
+    stderr.writeln('error: $path is a directory');
+  } else {
+    exitCode = 2;
+  }
+}
+
+
+
+
+
+
+
+
